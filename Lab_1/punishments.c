1396aa6a63076b61fccb9f85ec3073c142a48809
CynicalHacker <32186640+CynicalHacker@users.noreply.github.com>
2019-02-13 19:58:19 -0800

    Add files via upload

---
+++ Lab_1/punishments.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+	{
+	int a,b,c;	
+	printf("Enter the number of lines for the punishment: ");
+	scanf("%d",&a);
+		if (a <= 0){
+			printf("You entered an incorrect value for the number of lines!");
+			} else { 
+				printf("Enter the line for which we want to make a typo: ");
+					scanf("%d",&b);
+						if(b <= 0){ 
+							printf("You entered an incorrect value for the line typo!");
+					 	} else {	
+							for(c=1;c <= a; c = c+1){
+								if(c==b){
+									printf("C programming language is the bet!\n");
+								} else {
+									printf("C programming language is the best!\n");
+								}
+							}
+						}				
+
+				
+	
+	return 0;
+					}
+				}
